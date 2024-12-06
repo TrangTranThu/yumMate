ad47037aa2c0eda4092f4921c2ebb5a9aa9c427e
InternetGirl <trangtt181202@gmail.com>
2024-12-06 18:47:57 +0700

    mealActivity

--- app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.easyfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.example.easyfood.activities.MealActivity
 import com.example.easyfood.databinding.FragmentHomeBinding
 import com.example.easyfood.pojo.Meal
 import com.example.easyfood.viewModel.HomeViewModel
@@ -37,7 +39,17 @@ class HomeFragment : Fragment() {
 
         homeMvvm.getRandomMeal()
         observerRandomMeal()
+
+        obRandomMealClick()
     }
+
+    private fun obRandomMealClick() {
+        binding.randomMealCard.setOnClickListener{
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     //used to observer các thay đổi của data từ ViewModel -> cập nhật UI khi data thay đổi.
     private fun observerRandomMeal() {
         // homeMvvm: là instance của ViewModel
