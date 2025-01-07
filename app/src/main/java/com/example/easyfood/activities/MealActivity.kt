6e6e87b4b1dee67f86b040e0166c6ec31c0b0be3
InternetGirl <trangtt181202@gmail.com>
2025-01-07 18:28:58 +0700

    feat: layout Meal

---
+++ app/src/main/java/com/example/easyfood/activities/MealActivity.kt
@@ -0,0 +1,42 @@
+package com.example.easyfood.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.easyfood.R
+import com.example.easyfood.databinding.ActivityMealBinding
+import com.example.easyfood.databinding.FragmentHomeBinding
+import com.example.easyfood.fragments.HomeFragment
+
+class MealActivity : AppCompatActivity() {
+    private lateinit var mealId:String
+    private lateinit var mealName:String
+    private lateinit var mealThumb:String
+    private lateinit var binding: ActivityMealBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMealBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getMealInformationFromIntent()
+
+        setInformationInViews()
+    }
+
+    private fun setInformationInViews() {
+        Glide.with(applicationContext)
+            .load(mealThumb)
+            .into(binding.imgMealDetail)
+        binding.collapsingToolbar.title = mealName
+        binding.collapsingToolbar.setCollapsedTitleTextColor(resources.getColor(R.color.white))
+        binding.collapsingToolbar.setExpandedTitleColor(resources.getColor(R.color.white))
+    }
+
+    private fun getMealInformationFromIntent() {
+        val intent = intent
+        mealId = intent.getStringExtra(HomeFragment.Meal_ID)!!
+        mealName = intent.getStringExtra(HomeFragment.Meal_NAME)!!
+        mealThumb = intent.getStringExtra(HomeFragment.Meal_THUMB)!!
+    }
+
+}
