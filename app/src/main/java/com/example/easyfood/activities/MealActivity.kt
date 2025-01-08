e3b538b8598d1534cc02530073fb7aac97f0ca54
InternetGirl <trangtt181202@gmail.com>
2025-01-08 18:43:44 +0700

    feat: click btn_youtube move Youtube

--- app/src/main/java/com/example/easyfood/activities/MealActivity.kt
+++ app/src/main/java/com/example/easyfood/activities/MealActivity.kt
@@ -1,13 +1,15 @@
 package com.example.easyfood.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.easyfood.R
 import com.example.easyfood.databinding.ActivityMealBinding
-import com.example.easyfood.databinding.FragmentHomeBinding
 import com.example.easyfood.fragments.HomeFragment
 import com.example.easyfood.pojo.Meal
 import com.example.easyfood.viewModel.MealViewModel
@@ -17,8 +19,8 @@ class MealActivity : AppCompatActivity() {
     private lateinit var mealName:String
     private lateinit var mealThumb:String
     private lateinit var binding: ActivityMealBinding
+    private lateinit var youtubeLink: String
     private lateinit var mealMvvm: MealViewModel
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,18 +29,35 @@ class MealActivity : AppCompatActivity() {
 
         mealMvvm = ViewModelProvider(this)[MealViewModel::class.java]
         getMealInformationFromIntent()
+
         setInformationInViews()
+
+        loadingCase()
+
         mealMvvm.getMealDetail(mealId)
         observerMealDetailsLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
     }
 
     private fun observerMealDetailsLiveData() {
         mealMvvm.observerMealDealsLiveData().observe(this, object : Observer<Meal> {
             override fun onChanged(value: Meal) {
+                onResponseCase()
+
                 val meal = value
                 binding.tvCategory.text = "Category : ${meal!!.strCategory}"
                 binding.tvArea.text = "Are : ${meal.strArea}"
                 binding.tvInstructionsSteps.text = meal.strInstructions
+
+                youtubeLink = meal.strYoutube
             }
         })
     }
@@ -59,4 +78,22 @@ class MealActivity : AppCompatActivity() {
         mealThumb = intent.getStringExtra(HomeFragment.Meal_THUMB)!!
     }
 
+    private fun loadingCase() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnAddToFav.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.tvCategory.visibility = View.INVISIBLE
+        binding.tvArea.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+
+    }
+
+    private fun onResponseCase() {
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnAddToFav.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.tvCategory.visibility = View.VISIBLE
+        binding.tvArea.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
+    }
 }
